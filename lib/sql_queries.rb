12b52e9f4445f6422dd1b7adda1cf55067c258fb
Prince Charles <princecharles35@yahoo.com>
2019-11-04 21:06:50 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,8 +1,9 @@
 def selects_the_titles_of_all_projects_and_their_pledge_amounts
-"Write your SQL query Here"	  "SELECT projects.title, SUM(pledges.amount)
+"SELECT projects.title, SUM(pledges.amount)
   FROM projects
   INNER JOIN pledges
   ON projects.id = pledges.project_id
   GROUP BY projects.id
   ORDER BY projects.title;"
 end	end
+
