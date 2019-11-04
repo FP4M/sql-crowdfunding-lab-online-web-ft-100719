cc126f7397e69dcb83daff21507c76c6549884e4
Prince Charles <princecharles35@yahoo.com>
2019-11-04 21:08:00 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -7,3 +7,10 @@ def selects_the_titles_of_all_projects_and_their_pledge_amounts
   ORDER BY projects.title;"
 end	end
 
+def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
+"SELECT projects.title, SUM(pledges.amount) - projects.funding_goal
+    FROM projects
+    JOIN pledges ON projects.id = pledges.project_id
+    GROUP BY projects.title HAVING SUM(pledges.amount) >= projects.funding_goal;"
+end	end
+
